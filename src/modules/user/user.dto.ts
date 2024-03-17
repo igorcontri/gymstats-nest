04e82348e0657533de1013931429407b565f8391
Igor Contri <igorcontri@gmail.com>
2024-03-17 12:50:35 -0300

    feat(modules): add user dto

--- src/modules/user/user.dto.ts
+++ src/modules/user/user.dto.ts
@@ -1,11 +1,11 @@
-export type userDTO = {
-  id?: String;
-  name: String;
-  email: String;
-  password: String;
-  age?: Number;
-  weight?: Number;
-  Height?: Number;
+export type UserDTO = {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  age?: number;
+  weight?: number;
+  Height?: number;
   created_at?: Date;
   updated_at?: Date;
 };
