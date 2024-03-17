4497c648b060cad261a81a5e040d5872d982cc0b
Igor Contri <igorcontri@gmail.com>
2024-03-17 12:51:43 -0300

    add PrismaService

--- src/database/PrismaService.ts
+++ src/database/PrismaService.ts
@@ -1,7 +1,7 @@
-//conexão dos models com o database
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+//conexão/interação com banco de dados
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
